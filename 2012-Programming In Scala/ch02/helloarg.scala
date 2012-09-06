d61bcbc5f2ffbaa2cfd99feac61fa57975cf680a
SungJu Cho <scho@scho-mn.linkedin.biz>
2012-09-05 17:21:18 -0700

    Changes in the practice files

--- 2012-Programming In Scala/ch02/helloarg.scala
+++ 2012-Programming In Scala/ch02/helloarg.scala
@@ -1,1 +1,8 @@
+// Say hello to the first argument
 println("Hello, " + args(0) + "!")
+
+var i = 0
+while (i < args.length) {
+	println(args(i))
+	i += 1
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Book
 ====
+Study codes during book readings
