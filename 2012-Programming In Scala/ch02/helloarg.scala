8a733e814dc5df1183c702c1d06129830dbfc204
SungJu Cho <scho@scho-mn.linkedin.biz>
2012-09-06 18:00:24 -0700

    Modify the chapter 2 files.

--- 2012-Programming In Scala/ch02/helloarg.scala
+++ 2012-Programming In Scala/ch02/helloarg.scala
@@ -1,8 +1,2 @@
 // Say hello to the first argument
 println("Hello, " + args(0) + "!")
-
-var i = 0
-while (i < args.length) {
-	println(args(i))
-	i += 1
-}
--- 2012-Programming In Scala/ch02/pa.scala
+++ 2012-Programming In Scala/ch02/pa.scala
@@ -1,1 +1,1 @@
-args.foreach(arg => println(arg))
+args.foreach((arg: String) => println(arg))
