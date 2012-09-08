f1fc100fa0fc89df24a1dfb3519fae0ffd8fa6cc
SungJu Cho <scho@ppowar-mn.linkedin.biz>
2012-09-07 17:27:48 -0700

    Add Chapter 4.

---
+++ 2012-Programming In Scala/ch04/FallWinterSpringSummer.scala
@@ -0,0 +1,6 @@
+import ChecksumAccumulator.calculate
+
+object FallWinterSpringSummer extends Application {
+	for (season <- List("fall", "winter", "spring"))
+		println(season + ": " + calculate(season))
+}
---
+++ 2012-Programming In Scala/ch04/semicolon.scala
@@ -0,0 +1,12 @@
+val x = 1
+val y = 2
+
+val z = x +
+y +
+2
+
+val a = x
++ y 
++ 2
+println(z)
+println(a)
