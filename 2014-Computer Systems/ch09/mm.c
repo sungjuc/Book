32b4ac5afa4a6629ad079db023ae894b0d8f48dd
sungjuc <sungjuc73@gmail.com>
2014-03-09 19:09:59 -0700

    find_fit update

--- 2014-Computer Systems/ch09/mm.c
+++ 2014-Computer Systems/ch09/mm.c
@@ -1,15 +1,15 @@
 static char* heap_listp;
 
-static void *find_fit(size_t, asize)
+static void *find_fit(size_t asize)
 {
-    void *curr_ptr = heap_listp;
-    while (GET_SIZE(heap_listp) <= asize) {    
-        curr_ptr = NEXT_BLKP(curr_ptr);
-        if (curr_ptr == NULL)
-            curr_ptr = extend_heap(CHUNKSIZE/WSIZE);
-    }
+    void *bp;
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    } 
     
-    return curr_ptr;
+    return NULL;
 }    
 
 static void place(void *bp, size_t asize)
